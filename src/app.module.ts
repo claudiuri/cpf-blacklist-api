a357d1d1ce4afd19c21f09f41ce61f73884d4e7e
Cláudio Yuri <claudio.y07@gmail.com>
2022-01-10 19:06:37 -0300

    chore: TypeOrm initial setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CpfModule } from './cpf/cpf.module';
 
 @Module({
-  imports: [CpfModule],
+  imports: [
+    CpfModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
