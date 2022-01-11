d511fc932e388ed2977a622f9fddb82faf009484
Claudio Yuri <claudio.ferreira@cubos.io>
2022-01-11 20:31:33 -0300

    feat: add nestjs config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,13 +9,14 @@ import { LoggerModule } from './logger/logger.module';
 @Module({
   imports: [
     CpfModule,
+    LoggerModule,
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite3',
       autoLoadEntities: true,
       synchronize: true,
     }),
-    LoggerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 function buildSwaggerDoc(app: INestApplication) {
@@ -18,11 +19,17 @@ function buildSwaggerDoc(app: INestApplication) {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
+
+  const PORT = parseInt(configService.get('PORT')!) ?? 3000;
+
   app.useGlobalPipes(new ValidationPipe());
 
   buildSwaggerDoc(app);
 
-  await app.listen(3000);
+  await app.listen(PORT);
+
+  console.log(`App listening to port :${PORT}`);
   console.log(`App running on: ${await app.getUrl()}`);
 }
 bootstrap();
