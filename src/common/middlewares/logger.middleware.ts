fcc7707022e2a774e19b2d131eb3a47a1baffeb9
Claudio Yuri <claudio.ferreira@cubos.io>
2022-01-11 14:17:48 -0300

    feat: add logger service on logger middleware

--- src/common/middlewares/logger.middleware.ts
+++ src/common/middlewares/logger.middleware.ts
@@ -1,9 +1,13 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
+import { LoggerService } from 'src/logger/logger.service';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
+  constructor(private readonly loggerService: LoggerService) {}
+
   use(req: Request, res: Response, next: NextFunction) {
+    this.loggerService.sum();
     next();
   }
 }
