6007b23e87bd8b55eb454153575632ed7a9a6c62
Claudio Yuri <claudio.ferreira@cubos.io>
2022-01-12 13:07:16 -0300

    feat: update swagger route

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ function buildSwaggerDoc(app: INestApplication) {
 
   const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, document);
 }
 
 async function bootstrap() {
